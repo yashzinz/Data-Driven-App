6a39d699e36714e1fc51481dd5dfb54e52e938e6
Yashvi Soni <113103078+yashzinz@users.noreply.github.com>
2024-04-17 23:44:19 +0400

    adding comments

--- dataDrivenApp.py
+++ dataDrivenApp.py
@@ -1,10 +1,11 @@
 from customtkinter import *
 import cv2
 from PIL import Image, ImageTk
+from CTkMessagebox import CTkMessagebox
 from io import BytesIO
 import random
-import requests
-import webbrowser
+import requests #for requesting from api
+import webbrowser #for creating a hyperlink
 
 
 #creating class for setting up a video as background and playing it in loop. 
@@ -33,28 +34,28 @@ class VideoBg:
 root=CTk()
 root.geometry('420x595')
 root.title('Potter World App')
-from CTkMessagebox import CTkMessagebox
 
 #function to switch frames
 def switch_frame(frame):
     frame.tkraise()
-
+#function for messagebox for about button
 def aboutPopup():
     CTkMessagebox(title='About the app', message=f"This application explores the Harry potter world. Explore the 7 wonders of the Hp world in this application ", option_1='Let\'s go!')
-
+#function for messagebox for info button
 def infoPopup():
     CTkMessagebox(title='Navigation Info', message=f"To explore books, please click \"Get a Book\" button. It will generate one of the 7 Harry Potter titles. Click the button again to change the book title. Click \"View the Book\" to check the given title's book details", option_1='Let\'s go!', icon='question')
 
-
+#function for requesting data from potter api to use in the app
 def getBook():
     url = f"https://api.potterdb.com/v1/books"  
     response = requests.get(url)
     data = response.json()
-
+    #for generating a random integer in the range of (0-7) for generating a random title on click
     l_num = random.randint(0, len(data['data']))
 
-   
+    #for accessing the data from json file
     book_title = data['data'][l_num]['attributes']['title']
+    #for replacing the empty labels with data from json file
     bookLabel.configure(text=book_title)
     book_name.configure(text=f"Title\t  :  {book_title}")
 
@@ -69,17 +70,18 @@ def getBook():
 
     bookWiki = data['data'][l_num]['attributes']['wiki']
     from textwrap import wrap
-    bookwiki_new = '\n'.join(wrap(bookWiki, width=40))
+    bookwiki_new = '\n'.join(wrap(bookWiki, width=40)) #for breaking the long link string
     book_wiki.configure(text=f": {bookwiki_new}")
-    book_wiki.bind('<Button-1>', lambda x:webbrowser.open_new(f"\"{bookWiki}\""))
+    book_wiki.bind('<Button-1>', lambda x:webbrowser.open_new(f"\"{bookWiki}\"")) #for creating a hyperlink
     
+    #for requesting images from json file
     img_url = data['data'][l_num]['attributes']['cover']
     img_res = requests.get(img_url)
     if img_res.status_code == 200:
         img = CTkImage(Image.open(BytesIO(img_res.content)), size=(212, 303.5))
         cover_label = CTkLabel(home_frame, image=img, text="")
         cover_label.image = (
-            img  #keep a reference to the image to prevent garbage collection
+            img  #to prevent garbage collection
         )
         cover_label.place(x=99.5, y=164.7)
         
@@ -121,22 +123,26 @@ video_source = "Video assets\hp bg3.mp4"
 app = VideoBg(details_frame, video_source)
 
 
-#buttons on main frame. Explore button switches the frame to home frame. 
+#button on main frame, to give users an idea about the app
 aboutbtn = CTkButton(main_frame, text="About", command=aboutPopup ,fg_color="#751807", corner_radius=40, bg_color="#F59F14", text_color="#F59F14", hover_color="#b5250b", font=("Georgia", 20), width=150, height=50)
 aboutbtn.place(x=50, y=500)
-
+#button on main frame, to switch the frame to home frame
 explorebtn = CTkButton(main_frame, text="Explore", command= lambda:switch_frame(home_frame), fg_color="#751807", corner_radius=40, bg_color="#F59F14", text_color="#F59F14", hover_color="#b5250b", font=("Georgia", 20), width=150, height=50)
 explorebtn.place(x=220, y=500)
-#button on home frame
+
+#button on home frame, to generate a book title
+getbtn = CTkButton(home_frame, text=" Get a book " , command=getBook ,fg_color="#751807",  bg_color="#F59F14", text_color="#F59F14", hover_color="#b5250b", font=("Georgia", 20), width=150, height=50, corner_radius=0)
+getbtn.place(x=220, y=527.2)
+
+#button on home frame, to switch the frane to details frame
 viewbtn = CTkButton(home_frame, text=" View the Book ", command=lambda:[switch_frame(details_frame), getBook] ,fg_color="#751807",  bg_color="#F59F14", text_color="#F59F14", hover_color="#b5250b", font=("Georgia", 20), width=150, height=50, corner_radius=0)
 viewbtn.place(x=42, y=527.2)
 
-nextbtn = CTkButton(home_frame, text=" Get a book " , command=getBook ,fg_color="#751807",  bg_color="#F59F14", text_color="#F59F14", hover_color="#b5250b", font=("Georgia", 20), width=150, height=50, corner_radius=0)
-nextbtn.place(x=220, y=527.2)
-
+#empty label in home frame, to replace with title of the generated book
 bookLabel = CTkLabel(home_frame, text="", font=("Georgia", 14, 'bold'), text_color="#751807", bg_color="#F59F14")
 bookLabel.place(x=60, y=470)
 
+#empty labels on details frame, to replace with details of the generated book
 book_name =  CTkLabel(details_frame, text="", font=("Georgia", 14), text_color="#751807", bg_color="#F59F14")
 book_name.place(x=42, y=444.4)
 
@@ -155,15 +161,19 @@ book_wiki.place(x=120, y=544.8)
 wiki_label = CTkLabel(details_frame, text='Wiki',font=("Georgia", 14), text_color="#751807", bg_color="#F59F14" )
 wiki_label.place(x=42, y=540.8)
 
+# back button for switching between frames
 back=CTkImage(Image.open("Image Assets\Previous_icon.png"), size=(40, 40))
 backbtn=CTkButton(home_frame, image=back, text='', width=30 , fg_color="#F59F14",  bg_color="#F59F14", hover=False, command=lambda: switch_frame(main_frame))
 backbtn.place(x=17.1, y=23.4)
 backbtn2=CTkButton(details_frame, image=back, text='', width=30 , fg_color="#F59F14",  bg_color="#F59F14", hover=False, command=lambda: switch_frame(home_frame) )
 backbtn2.place(x=17.1, y=23.4)
 
+#info button for instructions on how to use the app
 info=CTkImage(Image.open("Image Assets\q_icon.png"), size=(40, 40))
 infobtn=CTkButton(home_frame, image=info, text='', width=30 , fg_color="#F59F14",  bg_color="#F59F14", hover=False, command= infoPopup )
 infobtn.place(x=359, y=23.4)
 
+#for starting with main frame on launch
 switch_frame(main_frame)
+
 root.mainloop()
